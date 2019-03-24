4511214e499a08c47622dcde4c26b2307ec1a4e1
Matas Graj <matas.graj@gmail.com>
2019-03-24 22:27:34 +0200

    exported class Student to new file

--- IPA_laborai_3_4/Program.cs
+++ IPA_laborai_3_4/Program.cs
@@ -4,27 +4,6 @@ using System.Linq;
 
 namespace IPA_laborai_3_4
 {
-    public class Student
-    {
-        public string Name;
-        public string Surname;
-        public double AvgResult;
-        public double MedianResult;
-        public bool IsInputFromFile;
-        public bool IsAvgSelected;
-
-        public Student(string vName, string vSurname, double vAvgResult, double vMedianResult, bool vIsInputFromFile,
-            bool vIsAvgSelected)
-        {
-            Name = vName;
-            Surname = vSurname;
-            AvgResult = vAvgResult;
-            MedianResult = vMedianResult;
-            IsInputFromFile = vIsInputFromFile;
-            IsAvgSelected = vIsAvgSelected;
-        }
-    }
-
     internal class Program
     {
         public static void Main(string[] args)
