f16c2f22efea3f46fd073540834964d18b638aa8
Matas Graj <matas.graj@gmail.com>
2019-03-24 10:54:25 +0200

    added Student class

--- IPA_laborai_3_4/Program.cs
+++ IPA_laborai_3_4/Program.cs
@@ -1,9 +1,26 @@
 ﻿namespace IPA_laborai_3_4
 {
+    public class Student
+         {
+             public string Name;
+             public string Surname;
+             public double Result;
+             public bool isAvgSelected;
+ 
+             public Student(string vName, string vSurname, double vResult, bool vIsAvgSelected)
+             {
+                 Name = vName;
+                 Surname = vSurname;
+                 Result = vResult;
+                 isAvgSelected = vIsAvgSelected;
+             }
+             
+         }
     internal class Program
     {
         public static void Main(string[] args)
         {
+            
         }
     }
 }
